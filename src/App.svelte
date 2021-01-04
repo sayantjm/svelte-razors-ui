184d6b7168600ded2d6ff1514fca4c2de110ded4
Juanma Perales <sayantjm@gmail.com>
2021-01-04 09:35:42 +0100

    added basic funtionality and navigation

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,28 @@
 <script>
-	export let name;
+	// pages
+	import Login from './pages/Login.svelte';
+	import About from './pages/About.svelte';
+	import Checkout from './pages/Checkout.svelte';
+	import Home from './pages/Home.svelte';
+	import Products from './pages/Products.svelte';
+	import ProductTemplate from './pages/ProductTemplate.svelte';
+
+	// router
+	import {Router, Route, Link} from 'svelte-routing';
 </script>
 
+<Router>
+	<nav class="navbar">
+		<Link to="/">Home</Link>
+		<Link to="/about">About</Link>
+		<Link to="/products">Products</Link>
+	</nav>
+	<Route path="/" component={Home} />
+	<Route path="/about" component={About} />
+	<Route path="/login" component={Login} />
+	<Route path="/products" component={Products} />
+	<Route path="/products/:id" component={ProductTemplate} />
+</Router>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello!</h1>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
